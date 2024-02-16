b87e97f512ffaa3c6d6a8a0adad9e6c0a41882ab
Ben Kahl <32250011+ben-kahl@users.noreply.github.com>
2024-02-15 19:39:42 -0800

    Moving to new repo
    Moving from Oregon-State-University/CS361/CS361-Term-Project

---
+++ CS361 Term Project/scripts/profile.gd
@@ -0,0 +1,14 @@
+extends Node
+
+@export var profile_info = {
+	"user_name" : "User_Name",
+	"bio" : "Test",
+	"control_options" : {
+		#TODO Implement user saveable controls
+		"config" : null
+	}
+	
+}
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
